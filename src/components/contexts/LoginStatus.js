32a5016de131d63c3952636f47c33853a4113e55
shinymidori <yinecaocye@gmail.com>
2026-07-13 19:04:37 -0700

    Delete src directory

--- src/components/contexts/LoginStatus.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-// Default context: not logged in, and a placeholder setter
-const LoginStatusContext = createContext([
-    { username: null },
-    () => { }
-]);
-
-export default LoginStatusContext;
